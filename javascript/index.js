8d459b775be917458335e1e41dd754b6168bfd58
Cesar <cesargb@gmail.com>
2023-01-13 19:15:39 +0100

    header

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,9 @@
-function toggleMenu() {
-  const submenuTypeAnimals = document.getElementById('submenu_type_animals')
-
-  submenuTypeAnimals.classList.toggle('hidden')
+function openMenu() {
+  console.log('open menu')
+  document.getElementById('submenu_type_animals').classList.remove('hidden')
 }
 
 function closeMenu() {
+  console.log('close menu')
   document.getElementById('submenu_type_animals').classList.add('hidden')
 }
